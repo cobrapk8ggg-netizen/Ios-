bd5770ef89d54d065e9c54af96ed983e9b0a43bc
ZEUS 2 <cobrapk8ggg@gmail.com>
2026-03-18 22:22:39 +0300

    Update api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://c-production-fba8.up.railway.app'; 
+const API_URL = 'https://c-production-f88c.up.railway.app'; 
 
 const api = axios.create({
   baseURL: API_URL,
